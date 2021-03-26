8743032d2cc613a50294ae40f50b37f12c47a607
Gwoat <77695380+Gwoat@users.noreply.github.com>
2021-03-26 12:09:19 -0700

    Create dot.py

---
+++ dot.py
@@ -0,0 +1,12 @@
+def factorial(x):
+
+    if x == 1:
+        return 1
+    else:
+        return(x * factorial(x-1))
+
+
+if __name__ == "__main__":
+
+    num = input("What do you want the factoril of?  ")
+    print("The factorial of " , num , "is" , factorial(int(num)))
